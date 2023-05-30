e09988997ced6445504dc2bd7bb7593d9b685aca
Yuri Lima <61335084+yurilima7@users.noreply.github.com>
2023-05-30 19:24:45 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [nome, setNome] = useState('');
@@ -8,6 +8,18 @@ function App() {
   const [tarefas, setTarefas] = useState([]);
 
   const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const tarefasStorage = localStorage.getItem('@tarefa');
+
+    if (tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage))
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('@tarefa', JSON.stringify(tarefas))
+  }, [tarefas]);
 
   function register(e) {
     e.preventDefault();
@@ -27,45 +39,45 @@ function App() {
       <h1>Cadastrando usuário</h1>
 
       <form onSubmit={register}>
-        <label>Nome:</label><br/>
-        <input 
-          placeholder='Digite seu nome' 
-          value={nome} 
-          onChange={ (e) => setNome(e.target.value) }
-        /><br/>
-        
-        <label>Email:</label><br/>
-        <input 
-          placeholder='Digite seu email' 
-          value={email} 
-          onChange={ (e) => setEmail(e.target.value) }
-        /><br/>
+        <label>Nome:</label><br />
+        <input
+          placeholder='Digite seu nome'
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        /><br />
 
-        <label>Idade:</label><br/>
-        <input 
-          placeholder='Digite sua idade' 
-          value={idade} 
-          onChange={ (e) => setIdade(e.target.value) }
-        /><br/>
+        <label>Email:</label><br />
+        <input
+          placeholder='Digite seu email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        /><br />
 
-        <label>Nome da tarefa:</label><br/>
-        <input 
-          placeholder='Digite uma tarefa' 
-          value={input} 
-          onChange={ (e) => setInput(e.target.value) }
-        /><br/>
+        <label>Idade:</label><br />
+        <input
+          placeholder='Digite sua idade'
+          value={idade}
+          onChange={(e) => setIdade(e.target.value)}
+        /><br />
+
+        <label>Nome da tarefa:</label><br />
+        <input
+          placeholder='Digite uma tarefa'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        /><br />
 
         <button type='submit'>Registrar</button>
       </form>
 
-      <br/><br/>
+      <br /><br />
 
       <div>
-        <span>Bem vindo: {user.nome}</span><br/>
-        <span>Idade: {user.idade}</span><br/>
-        <span>Email: {user.email}</span><br/>
+        <span>Bem vindo: {user.nome}</span><br />
+        <span>Idade: {user.idade}</span><br />
+        <span>Email: {user.email}</span><br />
         <ul>
-          {tarefas.map( tarefa => (
+          {tarefas.map(tarefa => (
             <li key={tarefa}>{tarefa}</li>
           ))}
         </ul>

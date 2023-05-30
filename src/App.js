86173bfc5abc970cfed9864f8d467f8cd4ae9da3
Yuri Lima <61335084+yurilima7@users.noreply.github.com>
2023-05-30 18:21:59 -0300

    Trabalhando com formulários

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
 import { useState } from 'react';
-import Nome from './components/Nome';
 
 function App() {
-  const [aluno, setAluno] = useState('Yuri Lima')
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [idade, setIdade] = useState(0);
 
-  function handleChangeName(nome) {
-    setAluno(nome)
+  const [user, setUser] = useState({});
+
+  function register(e) {
+    e.preventDefault();
+
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email,
+    })
   }
 
   return (
     <div>
-      <h1>Componente App</h1><br />
-      <h2>Olá: {aluno}</h2>
-      <button onClick={ () => handleChangeName('Yuri da Silva Lima') }>
-        Mudar Nome
-      </button>
+      <h1>Cadastrando usuário</h1>
+
+      <form onSubmit={register}>
+        <label>Nome:</label><br/>
+        <input 
+          placeholder='Digite seu nome' 
+          value={nome} 
+          onChange={ (e) => setNome(e.target.value) }
+        /><br/>
+        
+        <label>Email:</label><br/>
+        <input 
+          placeholder='Digite seu email' 
+          value={email} 
+          onChange={ (e) => setEmail(e.target.value) }
+        /><br/>
+
+        <label>Idade:</label><br/>
+        <input 
+          placeholder='Digite sua idade' 
+          value={idade} 
+          onChange={ (e) => setIdade(e.target.value) }
+        /><br/>
+
+        <button type='submit'>Registrar</button>
+      </form>
+
+      <br/><br/>
+
+      <div>
+        <span>Bem vindo: {user.nome}</span><br/>
+        <span>Idade: {user.idade}</span><br/>
+        <span>Email: {user.email}</span><br/>
+      </div>
     </div>
   );
 }

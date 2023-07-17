516dc25188331b0f87c56d11885dc07a1bf17954
Yuri Lima <61335084+yurilima7@users.noreply.github.com>
2023-07-17 11:11:44 -0300

    Buscando dados de um repositório e apresentando

--- repos/src/pages/Repository/repository.js
+++ repos/src/pages/Repository/repository.js
@@ -1,11 +1,62 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
+import { Container, Owner, Loading, BackButton } from "./styles";
+import api from "../../services/api";
 
 export default function Repository() {
     const name = useParams();
+
+    const [repository, setRepository] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        async function load() {
+            const nameRepo = decodeURIComponent(name.repository);
+
+            const [reposData, issuesData] = await Promise.all([
+                api.get(`/repos/${nameRepo}`),
+                api.get(`/repos/${nameRepo}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5,
+                    }
+                }),
+            ]);
+
+            console.log(reposData.data);
+            console.log(issuesData.data);
+            setRepository(reposData.data);
+            setIssues(issuesData.data)
+            setLoading(false);
+        }
+
+        load();
+    }, [name.repository]);
+
+    if (loading) {
+        return(
+            <Loading>
+                <h1>Carregando...</h1>
+            </Loading>
+        );
+    }
+    
     return(
-        <h1 style={{ color: "#fff" }}>
-            {decodeURIComponent(name.repository)}
-        </h1>
+        <Container>
+            <BackButton to="/">
+                <FaArrowLeft color="#000" size={30}/>
+            </BackButton>
+
+            <Owner>
+                <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
+
+                <h1>{repository.name}</h1>
+
+                <p>{repository.description}</p>
+            </Owner>
+        </Container>
     );
 }

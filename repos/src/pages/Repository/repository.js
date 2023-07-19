c5d2a568e95eb013f74ec330c4cd7ae2abe08198
Yuri Lima <61335084+yurilima7@users.noreply.github.com>
2023-07-19 10:04:31 -0300

    Exibindo issues

--- repos/src/pages/Repository/repository.js
+++ repos/src/pages/Repository/repository.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa";
-import { Container, Owner, Loading, BackButton } from "./styles";
+import { Container, Owner, Loading, BackButton, IssuesList } from "./styles";
 import api from "../../services/api";
 
 export default function Repository() {
@@ -57,6 +57,28 @@ export default function Repository() {
 
                 <p>{repository.description}</p>
             </Owner>
+
+            <IssuesList>
+                {issues.map(issue => (
+                    <li key={String(issue.id)}>
+                        <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+                        <div>
+                            <strong>
+                                <a href={issue.html_url}>
+                                    {issue.title}
+                                </a>
+
+                                {issue.labels.map(label => (
+                                    <span key={String(label.id)}>{label.name}</span>
+                                ))}
+                            </strong>
+
+                            <p>{issue.user.login}</p>
+                        </div>
+                    </li>
+                ))}
+            </IssuesList>
         </Container>
     );
 }
